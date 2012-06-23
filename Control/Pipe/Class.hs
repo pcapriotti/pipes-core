ab956029a6d9598cf9a761ff7050f3ff0a945f9a
Paolo Capriotti <p.capriotti@gmail.com>
2012-06-23 19:43:50 +0100

    Introduce PipeD and PipeL.

--- Control/Pipe/Class.hs
+++ Control/Pipe/Class.hs
@@ -1,16 +1,22 @@
-{-# LANGUAGE FlexibleInstances, TypeFamilies, KindSignatures #-}
+{-# LANGUAGE FlexibleInstances, TypeFamilies, KindSignatures, FlexibleContexts #-}
 {-# OPTIONS -fno-warn-orphans #-}
 
 module Control.Pipe.Class (
+  Monad3(..),
   MonadStream(..),
   MonadStreamDefer(..),
   MonadStreamUnawait(..),
-
+  PipeD,
+  withDefer,
+  PipeL,
   ) where
 
 import Control.Applicative
 import Control.Monad
 import Control.Monad.IO.Class
+import Control.Monad.Trans.Class
+import Control.Monad.Trans.Either
+import Control.Monad.Trans.State
 import Control.Pipe.Internal
 
 class Monad3 m where
@@ -21,7 +27,7 @@ instance Monad3 m => Monad (m a b u) where
   return = return3
   (>>=) = bind3
 
-class Monad3 m => MonadStream m where
+class (Monad3 m, Monad (BaseMonad m)) => MonadStream m where
   type BaseMonad m :: * -> *
 
   -- | Wait for input from upstream within the 'Pipe' monad.
@@ -42,8 +48,12 @@ class MonadStream m => MonadStreamDefer m where
   await :: MonadStreamDefer m => m a b u a
   await = awaitE >>= either defer return
 
+  runDefer :: m a b r r -> Pipe (BaseMonad m) a b r r
+
 class MonadStream m => MonadStreamUnawait m where
   unawait :: a -> m a b u ()
+
+  runUnawait :: m a b u r -> Pipe (BaseMonad m) a b u r
 
 instance Monad m => MonadStream (Pipe m) where
   type BaseMonad (Pipe m) = m
@@ -66,12 +76,59 @@ instance Monad m => Monad3 (Pipe m) where
   Yield x p w `bind3` f = Yield x (p `bind3` f) w
   Throw e p w `bind3` f = Throw e (p `bind3` f) w
 
-instance Monad m => Functor (Pipe m a b u) where
+instance Monad3 m => Functor (m a b u) where
   fmap = liftM
 
-instance Monad m => Applicative (Pipe m a b u) where
+instance Monad3 m => Applicative (m a b u) where
   pure = return
   (<*>) = ap
 
 instance MonadIO m => MonadIO (Pipe m a b u) where
   liftIO = execP Unmasked . liftIO
+
+-- PipeD
+
+newtype PipeD m a b u r = PipeD
+  { unPipeD :: EitherT u (Pipe m a b u) r }
+
+instance Monad m => Monad3 (PipeD m) where
+  return3 r = PipeD $ return r
+  bind3 (PipeD m) f = PipeD $ m >>= unPipeD . f
+
+instance Monad m => MonadStream (PipeD m) where
+  type BaseMonad (PipeD m) = m
+
+  awaitE = liftPipe awaitE
+  yield = liftPipe . yield
+  liftPipe = PipeD . lift
+
+instance Monad m => MonadStreamDefer (PipeD m) where
+  defer = PipeD . hoistEither . Left
+  runDefer = liftM (either id id) . runEitherT . unPipeD
+
+withDefer :: MonadStream m => PipeD (BaseMonad m) a b r r -> m a b r r
+withDefer = liftPipe . runDefer
+
+-- PipeL
+
+newtype PipeL m a b u r = PipeL
+  { unPipeL :: StateT [a] (Pipe m a b u) r }
+
+instance Monad m => Monad3 (PipeL m) where
+  return3 r = PipeL $ return r
+  bind3 (PipeL m) f = PipeL $ m >>= unPipeL . f
+
+instance Monad m => MonadStream (PipeL m) where
+  type BaseMonad (PipeL m) = m
+
+  awaitE = PipeL $ get >>= \stack -> case stack of
+    [] -> lift awaitE
+    (x : xs) -> put xs >> return (Right x)
+
+  yield = liftPipe . yield
+  liftPipe = PipeL . lift
+
+instance Monad m => MonadStreamUnawait (PipeL m) where
+  unawait = PipeL . modify . (:)
+
+  runUnawait = (`evalStateT` []) . unPipeL
--- Control/Pipe/Combinators.hs
+++ Control/Pipe/Combinators.hs
@@ -15,23 +15,22 @@ module Control.Pipe.Combinators (
   -- in the output type, to encourage their use in the implementation of
   -- higher-level combinators.
   fold,
---  fold1,
+  fold1,
   consume,
---  consume1,
---  -- ** List-like pipe combinators
---  -- The following combinators are analogous to the corresponding list
---  -- functions, when the stream of input values is thought of as a (potentially
---  -- infinite) list.
---  take,
---  drop,
---  takeWhile,
---  takeWhile_,
+  consume1,
+  -- ** List-like pipe combinators
+  -- The following combinators are analogous to the corresponding list
+  -- functions, when the stream of input values is thought of as a (potentially
+  -- infinite) list.
+  take,
+  drop,
+  takeWhile,
+  takeWhile_,
 --  dropWhile,
---  intersperse,
 --  groupBy,
---  filter,
---  -- ** Other combinators
---  pipeList,
+  filter,
+  -- ** Other combinators
+  pipeList,
   nullP,
   feed,
   ) where
@@ -40,6 +39,7 @@ import Control.Applicative
 import Control.Monad
 import Control.Pipe
 import Control.Pipe.Internal
+import Control.Pipe.Class
 import Data.Maybe
 import Prelude hiding (until, take, drop, concatMap, filter, takeWhile, dropWhile, catch)
 
@@ -49,83 +49,83 @@ import Prelude hiding (until, take, drop, concatMap, filter, takeWhile, dropWhil
 -- Further calls to 'tryAwait' after upstream termination will keep returning
 -- 'Nothing', whereas calling 'await' will terminate the current pipe
 -- immediately.
-tryAwait :: Monad m => Pipe m a b u (Maybe a)
+tryAwait :: MonadStream m => m a b u (Maybe a)
 tryAwait = awaitE >>= either (\_ -> return Nothing) (return . Just)
 
 -- | Successively yield elements of a list.
-fromList :: Monad m => [a] -> Pipe m x a u ()
+fromList :: MonadStream m => [a] -> m x a u ()
 fromList = mapM_ yield
 
 -- | A pipe that terminates immediately.
-nullP :: Monad m => Pipe m a b u ()
+nullP :: Monad3 m => m a b u ()
 nullP = return ()
 
 -- | A fold pipe. Apply a b uinary function to successive input values and an
 -- accumulator, and return the final result.
-fold :: Monad m => (b -> a -> b) -> b -> Pipe m a x u b
+fold :: MonadStream m => (b -> a -> b) -> b -> m a x u b
 fold f = go
   where
-    go x = tryAwait >>= maybe (return x) (let y = f x in y `seq` go . y)
+    go x = awaitE >>= either (\_ -> return x) (let y = f x in y `seq` go . y)
 
--- -- | A variation of 'fold' without an initial value for the accumulator. This
--- -- pipe doesn't return any value if no input values are received.
--- fold1 :: Monad m => (a -> a -> a) -> Pipe a x u m a
--- fold1 f = tryAwait >>= maybe discard (fold f)
+-- | A variation of 'fold' without an initial value for the accumulator. This
+-- pipe doesn't return any value if no input values are received.
+fold1 :: MonadStreamDefer m => (a -> a -> a) -> m a x u a
+fold1 f = await >>= fold f
 
 -- | Accumulate all input values into a list.
-consume :: Monad m => Pipe m a x u [a]
-consume = pipe (:) >+> (fold (.) id <*> pure [])
+consume :: MonadStream m => m a x u [a]
+consume = fold (\xs x -> xs . (x:)) id <*> pure []
 
--- -- | Accumulate all input values into a non-empty list.
--- consume1 :: Monad m => Pipe a x u m [a]
--- consume1 = pipe (:) >+> (fold1 (.) <*> pure [])
+-- | Accumulate all input values into a non-empty list.
+consume1 :: MonadStreamDefer m => m a x u [a]
+consume1 = (:) <$> await <*> consume
 
--- -- | Act as an identity for the first 'n' values, then terminate.
--- take :: Monad m => Int -> Pipe a a u m ()
--- take n = replicateM_ n $ await >>= yield
--- 
--- -- | Remove the first 'n' values from the stream, then act as an identity.
--- drop :: Monad m => Int -> Pipe a a u m r
--- drop n = replicateM_ n await >> idP
--- 
--- -- | Apply a function with multiple return values to the stream.
--- pipeList :: Monad m => (a -> [b]) -> Pipe m a b u r
--- pipeList f = forever $ await >>= mapM_ yield . f
--- 
--- -- | Act as an identity until as long as inputs satisfy the given predicate.
--- -- Return the first element that doesn't satisfy the predicate.
--- takeWhile :: Monad m => (a -> Bool) -> Pipe a a u m a
--- takeWhile p = go
---   where
---     go = await >>= \x -> if p x then yield x >> go else return x
--- 
--- -- | Variation of 'takeWhile' returning @()@.
--- takeWhile_ :: Monad m => (a -> Bool) -> Pipe a a u m ()
--- takeWhile_ = void . takeWhile
--- 
+-- | Act as an identity for the first 'n' values, then terminate.
+take :: MonadStreamDefer m => Int -> m a a u ()
+take n = replicateM_ n $ await >>= yield
+
+-- | Remove the first 'n' values from the stream, then act as an identity.
+drop :: MonadStream m => Int -> m a a r r
+drop n = withDefer $ replicateM_ n await >> idP
+
+-- | Apply a function with multiple return values to the stream.
+pipeList :: MonadStream m => (a -> [b]) -> m a b r r
+pipeList f = forP $ mapM_ yield . f
+
+-- | Act as an identity until as long as inputs satisfy the given predicate.
+-- Return the first element that doesn't satisfy the predicate.
+takeWhile :: MonadStreamDefer m => (a -> Bool) -> m a a u a
+takeWhile p = go
+  where
+    go = await >>= \x -> if p x then yield x >> go else return x
+
+-- | Variation of 'takeWhile' returning @()@.
+takeWhile_ :: MonadStreamDefer m => (a -> Bool) -> m a a u ()
+takeWhile_ = void . takeWhile
+
 -- -- | Remove inputs as long as they satisfy the given predicate, then act as an
 -- -- identity.
--- dropWhile :: Monad m => (a -> Bool) -> Pipe a a u m r
--- dropWhile p = (takeWhile p >+> discard) >>= yield >> idP
--- 
--- -- | Group input values by the given predicate.
--- groupBy :: Monad m => (a -> a -> Bool) -> Pipe a [a] m r
+-- dropWhile :: MonadStream m => (a -> Bool) -> m a a r r
+-- dropWhile p = liftPipe $ (withDefer (takeWhile p) >+> discard) >>= yield >> idP
+
+-- | Group input values by the given predicate.
+-- groupBy :: MonadStream m => (a -> a -> Bool) -> m a [a] u r
 -- groupBy p = streaks >+> createGroups
 --   where
 --     streaks = await >>= \x -> yield (Just x) >> streaks' x
---     streaks' x = do
+--     streaks' x = withDefer $ do
 --       y <- await
 --       unless (p x y) $ yield Nothing
 --       yield $ Just y
 --       streaks' y
---     createGroups = forever $
+--     createGroups = withDefer . forever $
 --       takeWhile_ isJust >+>
 --       pipe fromJust >+>
 --       (consume1 >>= yield)
--- 
--- -- | Remove values from the stream that don't satisfy the given predicate.
--- filter :: Monad m => (a -> Bool) -> Pipe a a u m r
--- filter p = forever $ takeWhile_ p
+
+-- | Remove values from the stream that don't satisfy the given predicate.
+filter :: MonadStream m => (a -> Bool) -> Pipe (BaseMonad m) a a r r
+filter p = withDefer . forever $ takeWhile p
 
 -- | Feed an input element to a pipe.
 feed :: Monad m => a -> Pipe m a b u r -> Pipe m a b u r
